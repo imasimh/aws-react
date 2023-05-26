9f61f025591c2a018fd179fc153f72120bad3de2
Asim Hussain <asimofficial1998@gmail.com>
2023-05-26 14:12:57 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -5,17 +5,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Deployment on Aws 
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://klaspad.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Klaspad
         </a>
       </header>
     </div>
